1bc65897200d2691b826f73d8e7a77eb1a53b61b
Vaibhav Kumar <126766355+vaibhavkumar2001@users.noreply.github.com>
2025-06-29 10:34:55 +0530

    Add implementation for Two Sum problem in C++

---
+++ Array/TwoSum.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hash;
+        for (int i = 0; i < nums.size(); i++) {
+            int result = target - nums[i];
+            if (hash.find(result) != hash.end()) {
+                return {hash[result], i};
+            }
+            hash[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    
+    vector<int> result = sol.twoSum(nums, target);
+    
+    if (!result.empty()) {
+        cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    } else {
+        cout << "No two numbers found that add up to the target." << endl;
+    }
+
+    return 0;
+}
