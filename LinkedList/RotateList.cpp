588ce12aeac16d1033f22a40589ddd650d5b548f
Vaibhav Kumar <126766355+vaibhavkumar2001@users.noreply.github.com>
2025-06-30 13:49:25 +0530

    Add implementation for rotating a linked list to the right in C++

---
+++ LinkedList/RotateList.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>    
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+}; 
+class Solution {
+public:
+    int getLength(ListNode* head) {
+        int len = 0;
+        while(head) {
+            len++;
+            head = head->next;
+        }
+        return len;
+    }
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head == NULL) {
+            return 0;
+        }
+        int len = getLength(head);
+        int actualRotateK = (k % len);
+        if(actualRotateK == 0) {
+            return head;
+        }
+        int newLastNodepos = len - actualRotateK - 1;
+        ListNode* newLastNode = head;
+        for(int i = 0;i < newLastNodepos;i++) {
+            newLastNode = newLastNode->next;
+        }
+        ListNode* newHead = newLastNode->next;
+        newLastNode->next = 0;
+        ListNode* it = newHead;
+        while(it->next) {
+            it = it->next;
+        }
+        it->next = head;
+        return newHead;
+    }
+};
